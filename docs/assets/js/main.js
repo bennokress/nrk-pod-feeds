dd85f8476aa133ee74f7af3ba9b3ece4e352a5c9
Benno Kress <developer+git-commit@bennokress.de>
2026-01-14 09:16:28 +0100

    feat: Add NRK TV video podcast support
    Add support for generating video podcast RSS feeds from NRK TV series using Podcasting 2.0 format. Initial configuration includes Dagsrevyen 21, Dagsrevyen and Dagsrevyen for utlandet. Video feeds use HLS streams (application/vnd.apple.mpegurl) which are only compatible with select podcatchers.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -2,14 +2,24 @@ function copyToClipboard(feed) {
     var copyText = document.getElementById("feed_url_" + feed);
 
     copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices 
+    copyText.setSelectionRange(0, 99999); // For mobile devices
 
     navigator.clipboard.writeText(copyText.value);
     console.log(`Copied value to clipboard: ${copyText.value}`)
 }
 
+function copyVideoToClipboard(feed) {
+    var copyText = document.getElementById("video_feed_url_" + feed);
+
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(copyText.value);
+    console.log(`Copied video feed URL: ${copyText.value}`)
+}
+
 function listFeeds() {
-    base_url = "https://sindrel.github.io/nrk-pod-feeds/rss/";
+    base_url = "https://bennokress.github.io/nrk-pod-feeds/rss/";
     info_base_url = "https://radio.nrk.no/podkast/";
 
     feeds.forEach(feed => {
@@ -32,8 +42,41 @@ function listFeeds() {
 
         feed_url = base_url + feed["id"] + ".xml";
         item = `<li><h4><font color="${stateColor}"><sup>${stateMsg}</sup></font> <a href="${info_base_url}${feed["id"]}" target="_blank">${feed["title"]}</a><br/><input type="text" size="40" value="${feed_url}" id="feed_url_${feed["id"]}" disabled> <button onclick="copyToClipboard('${feed["id"]}')">Copy</button><h3></li>`;
-        
+
         document.getElementById("feeds_list").innerHTML += item;
+    });
+}
+
+function listVideoFeeds() {
+    video_base_url = "https://bennokress.github.io/nrk-pod-feeds/rss/video/";
+    info_base_url = "https://tv.nrk.no/serie/";
+
+    if (typeof videoFeeds === 'undefined') {
+        return;  // No video feeds loaded
+    }
+
+    videoFeeds.forEach(feed => {
+        stateColor = "#338500"
+        stateMsg = "⬈"
+
+        if(feed["hidden"]) {
+            return;
+        }
+
+        if(!feed["enabled"]) {
+            stateColor = "#eb8904"
+            stateMsg = "⬊"
+        }
+
+        if(feed["ignore"]) {
+            stateColor = "#333"
+            stateMsg = "⬊"
+        }
+
+        feed_url = video_base_url + feed["id"] + ".xml";
+        item = `<li><h4><font color="${stateColor}"><sup>${stateMsg}</sup></font> <a href="${info_base_url}${feed["id"]}" target="_blank">${feed["title"]}</a><br/><input type="text" size="40" value="${feed_url}" id="video_feed_url_${feed["id"]}" disabled> <button onclick="copyVideoToClipboard('${feed["id"]}')">Copy</button><h3></li>`;
+
+        document.getElementById("video_feeds_list").innerHTML += item;
     });
 }
 
@@ -42,6 +85,24 @@ function searchFeeds() {
   input = document.getElementById('searchInput');
   filter = input.value.toUpperCase();
   ul = document.getElementById("feeds_ul");
+  li = ul.getElementsByTagName('li');
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+function searchVideoFeeds() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('videoSearchInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("video_feeds_ul");
   li = ul.getElementsByTagName('li');
 
   for (i = 0; i < li.length; i++) {
